71dee0e3643dc6f5ff4724ba50ad056db0445ff0
Pixel <ipixelsun@gmail.com>
2020-06-25 12:54:52 -0700

    temp token change

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import tweepy
 from time import sleep
 from random import randint 
 
-
-consumer_key = environ['consumer_key'] 
-consumer_secret = environ['consumer_secret']  
-access_token = environ['access_token'] 
-access_token_secret = environ['access_token_secret'] 
+consumer_key = "QvPyBLxlIFMi5bQJ8FEtUyiSd" 
+consumer_secret = "8ayGe7kd2qLFo8ffU1WXbJ5UEGBIZDuGIVk1jwluLxWgCBOwCK"     
+access_token = "1250597054302355456-mv5EtZUh8RCLnvjmv6TVupD5k3bIid"     
+access_token_secret = "AxrdOT5tShF3ZXvVtomgPybdYMZek0UC1A9OnehJdZygI" 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
