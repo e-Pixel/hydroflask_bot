acf9b8a1ef8adc1faef6644e30ea38a8f96660c7
Pixel <ipixelsun@gmail.com>
2020-06-30 00:21:11 -0700

    Tokens eliminados
    Los dejé para subir el proyecto a Heroku, ya no hace falta esto puesto que lo subiré directamente por el CLI. Eliminé los tokens por motivos de seguridad

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import tweepy
 from time import sleep
 from random import randint 
 
-consumer_key = "QvPyBLxlIFMi5bQJ8FEtUyiSd" 
-consumer_secret = "8ayGe7kd2qLFo8ffU1WXbJ5UEGBIZDuGIVk1jwluLxWgCBOwCK"     
-access_token = "1250597054302355456-mv5EtZUh8RCLnvjmv6TVupD5k3bIid"     
-access_token_secret = "AxrdOT5tShF3ZXvVtomgPybdYMZek0UC1A9OnehJdZygI"
+consumer_key = None # security
+consumer_secret = None
+access_token = None 
+access_token_secret = None
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
